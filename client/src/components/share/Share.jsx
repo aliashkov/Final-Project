0f6a2e16dc331fd3e86af42f1bbeede9074d1bc4
aliashkov <art1699584@gmail.com>
2022-05-19 14:48:55 +0300

    UI(client)

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import './share.css'
+import { PermMedia } from '@mui/icons-material';
 
 
 const Share = () => {
     return (
-        <div className='rightbar'> Rightbar</div>
+        <div className='share'>
+            <div className="shareWrapper">
+                <div className="shareTop">
+                    <img className='shareProfileImg' src="/assets/person/2.jpg" alt="" />
+                    <input
+                        placeholder='Input yput thoughts'
+                        className="shareInput"
+                    />
+                </div>
+                <hr className="shareHr" />
+                <div className="shareBottom">
+                    <div className="shareOptions">
+                        <div className="shareOption">
+                            <PermMedia htmlColor='tomato' className='shareIcon'/>
+                            <span className='shareOptionText'>Photo</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
