4402c8c3f5e1e69544f2c3f25383da37af441b0d
aliashkov <art1699584@gmail.com>
2022-05-21 00:13:10 +0300

    06 - UI

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import './rightbar.css'
+import "./rightbar.css";
+import { Users } from "../../data";
 
-
-const Rightbar = () => {
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
     return (
-        <div className='rightbar'> </div>
-    )
-}
+      <>
+        <h4 className="rightbarTitle">Friends</h4>
+        <ul className="rightbarFriendList">
+            <li className="rightbarFriend">
+                <div className="rightbarProfileImgContainer">
+                    <img className="rightbarProfileImg" src="assets/person/1.jpg" alt="" />
+                </div>
+                <span className="rightbarUsername">Vitaliy </span>
+            </li>
+            <li className="rightbarFriend">
+                <div className="rightbarProfileImgContainer">
+                    <img className="rightbarProfileImg" src="assets/person/1.jpg" alt="" />
+                </div>
+                <span className="rightbarUsername">Vitaliy </span>
+            </li>
+            <li className="rightbarFriend">
+                <div className="rightbarProfileImgContainer">
+                    <img className="rightbarProfileImg" src="assets/person/1.jpg" alt="" />
+                </div>
+                <span className="rightbarUsername">Vitaliy </span>
+            </li>
+            <li className="rightbarFriend">
+                <div className="rightbarProfileImgContainer">
+                    <img className="rightbarProfileImg" src="assets/person/1.jpg" alt="" />
+                </div>
+                <span className="rightbarUsername">Vitaliy </span>
+            </li>
+        </ul>
+      </>
+    );
+  };
 
-export default Rightbar;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+      <HomeRightbar/>
+      </div>
+    </div>
+  );
+}
