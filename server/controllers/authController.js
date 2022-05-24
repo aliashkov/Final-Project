811666d920efb8181edd4f32e01465d29d1feccb
aliashkov <art1699584@gmail.com>
2022-05-24 11:10:48 +0300

    test

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -24,14 +24,14 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     try {
         const user = await User.findOne({ email: req.body.email });
-        !user && res.status(404).json("user not found");
+        !user && res.status(404).json("User not found");
 
         const validPassword = await bcrypt.compare(req.body.password, user.password)
-        !validPassword && res.status(400).json("wrong password")
+        !validPassword && res.status(400).json("Wrong password")
 
         res.status(200).json(user)
     } catch (err) {
-        res.status(500).json(err)
+        console.log(err)
     }
 }
 
